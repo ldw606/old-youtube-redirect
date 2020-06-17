d68d1b807fea94b801d59df272fd30a2ccc6db70
BuildTools <unconfigured@null.spigotmc.org>
2020-06-17 19:29:50 +0100

    Redirect pages that already have a query string such as video pages

--- background.js
+++ background.js
@@ -1,13 +1,20 @@
-const queryString = "?disable_polymer=1";
+const queryString = "disable_polymer=1";
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    if(details.url.indexOf("?") == -1){
-      return {
-        redirectUrl: details.url + queryString
-      };
-    }else{
+    if(details.url.indexOf("disable_polymer") > -1){
+      //Don't infinitely redirect
       return {}
+    }else if(details.url.indexOf("?") == -1){
+      //Make a query string if one doesn't already exist
+      return {
+        redirectUrl: details.url + "?" + queryString
+      };
+    }else {
+      //If a query string already exists add to the end
+      return {
+        redirectUrl: details.url + "&" + queryString
+      };
     }
   },
   {
